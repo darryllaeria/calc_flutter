9b61bd59047689376647a0e0fe2158e37920e731
Darryll Aeria <darryllaeria@gmail.com>
2021-01-10 15:00:45 +0700

    Added first button

--- lib/main.dart
+++ lib/main.dart
@@ -22,10 +22,79 @@ class CalcHomePage extends StatefulWidget {
 }
 
 class _CalcHomePageState extends State<CalcHomePage> {
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold (
+  Widget buildButton(
+      String buttonText, double buttonHeight, Color buttonColor) {
+    return Container(
+        height: MediaQuery.of(context).size.height * .1 * buttonHeight,
+        color: Colors.redAccent,
+        child: FlatButton(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0.0),
+                side: BorderSide(
+                    color: Colors.white, width: 1, style: BorderStyle.solid)),
+            padding: EdgeInsets.all(16.0),
+            onPressed: null,
+            child: Text(buttonText,
+                style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white))));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(title: Text("Flutter Calculator")),
-      );
-    }
+        body: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+              child: Text(
+                "0",
+                style: TextStyle(fontSize: 38.0),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+              child: Text(
+                "0",
+                style: TextStyle(fontSize: 48.0),
+              ),
+            ),
+            Expanded(child: Divider()),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                    width: MediaQuery.of(context).size.width * .75,
+                    child: Table(
+                      children: [
+                        TableRow(children: [
+                          Container(
+                              height: MediaQuery.of(context).size.height * .1,
+                              color: Colors.redAccent,
+                              child: FlatButton(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(0.0),
+                                      side: BorderSide(
+                                          color: Colors.white,
+                                          width: 1,
+                                          style: BorderStyle.solid)),
+                                  padding: EdgeInsets.all(16.0),
+                                  onPressed: null,
+                                  child: Text("C",
+                                      style: TextStyle(
+                                          fontSize: 30.0,
+                                          fontWeight: FontWeight.normal,
+                                          color: Colors.white))))
+                        ])
+                      ],
+                    )),
+              ],
+            ),
+          ],
+        ));
+  }
 }
